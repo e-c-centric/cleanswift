584153e0e9b2ebd6527760f0a8b5e6314a8fea9a
Elikem Asudo <elikem.gale-zoyiku@ashesi.edu.gh>
2024-11-22 17:36:55 +0000

    db

--- db/cleanswift.sql
+++ db/cleanswift.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 16, 2024 at 11:53 PM
+-- Generation Time: Nov 22, 2024 at 06:36 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -31,6 +31,13 @@ CREATE TABLE `admins` (
   `admin_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`admin_id`) VALUES
+(9);
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +52,13 @@ CREATE TABLE `cart` (
   `added_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`cart_id`, `customer_id`, `service_id`, `quantity`, `added_at`) VALUES
+(6, 15, 5, 3, '2024-11-22 17:32:23');
+
 -- --------------------------------------------------------
 
 --
@@ -55,8 +69,39 @@ CREATE TABLE `customers` (
   `customer_id` int(11) NOT NULL,
   `customer_country` varchar(100) NOT NULL,
   `customer_city` varchar(100) NOT NULL,
-  `customer_image` varchar(100) DEFAULT NULL
+  `customer_image` longblob DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`customer_id`, `customer_country`, `customer_city`, `customer_image`) VALUES
+(15, 'Ghana', 'CG-1579-2818', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `deliveries`
+--
+
+CREATE TABLE `deliveries` (
+  `delivery_id` int(11) NOT NULL,
+  `driver_id` int(11) DEFAULT NULL,
+  `customer_id` int(11) NOT NULL,
+  `delivery_status` enum('With Customer','Transit to laundry','Transit from laundry','Returned to customer') DEFAULT 'With Customer',
+  `pickup_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `dropoff_time` datetime NOT NULL,
+  `cost` decimal(10,2) NOT NULL,
+  `provider_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `deliveries`
+--
+
+INSERT INTO `deliveries` (`delivery_id`, `driver_id`, `customer_id`, `delivery_status`, `pickup_time`, `dropoff_time`, `cost`, `provider_id`) VALUES
+(1, NULL, 15, 'With Customer', '2024-11-22 18:32:00', '2024-11-22 19:32:00', 0.00, 8);
 
 -- --------------------------------------------------------
 
@@ -69,6 +114,14 @@ CREATE TABLE `drivers` (
   `license_number` varchar(50) NOT NULL,
   `vehicle_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `drivers`
+--
+
+INSERT INTO `drivers` (`driver_id`, `license_number`, `vehicle_id`) VALUES
+(11, 'GHA-72448-1494-9', 1),
+(14, 'GHA-72448-1494-0', 3);
 
 -- --------------------------------------------------------
 
@@ -127,6 +180,26 @@ CREATE TABLE `providers` (
   `provider_address` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `providers`
+--
+
+INSERT INTO `providers` (`provider_id`, `provider_name`, `provider_address`) VALUES
+(8, 'LaundrySharp', 'CG-772-9085');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ratings`
+--
+
+CREATE TABLE `ratings` (
+  `rating_id` int(11) NOT NULL,
+  `business_id` int(11) DEFAULT NULL,
+  `rating_val` int(11) NOT NULL,
+  `comment` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- --------------------------------------------------------
 
 --
@@ -137,6 +210,16 @@ CREATE TABLE `roles` (
   `role_id` int(11) NOT NULL,
   `role_name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`role_id`, `role_name`) VALUES
+(4, 'admin'),
+(1, 'customer'),
+(2, 'driver'),
+(3, 'provider');
 
 -- --------------------------------------------------------
 
@@ -150,6 +233,15 @@ CREATE TABLE `services` (
   `service_name` varchar(100) NOT NULL,
   `price` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `services`
+--
+
+INSERT INTO `services` (`service_id`, `provider_id`, `service_name`, `price`) VALUES
+(5, 8, 'Upholstering of hat (per 5)', 7.00),
+(6, 8, 'Regular laundry (per item)', 2.00),
+(7, 8, 'Scrubbing shoes', 50.00);
 
 -- --------------------------------------------------------
 
@@ -168,6 +260,17 @@ CREATE TABLE `users` (
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_name`, `user_email`, `user_password`, `user_contact`, `role_id`, `created_at`, `updated_at`) VALUES
+(8, 'Emmanuel Agyei', 'agyei.emmanuel@ashesi.edu.gh', '$2y$10$Fy34r0Odc6qgyZKG0o3oOutihWnfdWK9eKvV//5Y0nixAGmpMBDmC', '+233552816008', 3, '2024-11-17 02:39:52', '2024-11-17 02:39:52'),
+(9, 'Super Man', 'superman@ashesi.edu.gh', '$2y$10$5FpfvkmgnIJmzmjLg3wrOuJWx8ErteftrZA8VKrMm/7QFF.hyNTLm', '+233552816008', 4, '2024-11-17 02:44:29', '2024-11-17 02:44:29'),
+(11, 'Test Driver', 'testdriver@ashesi.edu.gh', '$2y$10$F0kic6swT0vvrsXbRJ4EyOU6O2l9PQOQikuR2jNgW48NRwp5TktQK', '+233552816008', 2, '2024-11-17 04:43:53', '2024-11-17 04:43:53'),
+(14, 'Test User Driver', 'testuserdriver@gmail.com', '$2y$10$WGzRee6n9sx7W3vQqGHrG.7JzlwNF0w2XqR4buSBAUYkvUnz5u3z2', '+233507586382', 2, '2024-11-22 14:35:15', '2024-11-22 14:35:15'),
+(15, 'Elikem Gale-Zoyiku', 'egalezoyiku@gmail.com', '$2y$10$xyIHGUrGChIg2NpnFnoHQubvVAhUoWdGIFDYnE5eCm5Ioxzv4bD2y', '+233552816008', 1, '2024-11-22 15:01:07', '2024-11-22 15:01:07');
+
 -- --------------------------------------------------------
 
 --
@@ -177,8 +280,37 @@ CREATE TABLE `users` (
 CREATE TABLE `vehicles` (
   `vehicle_id` int(11) NOT NULL,
   `vehicle_number` varchar(50) NOT NULL,
-  `vehicle_type` varchar(50) NOT NULL
+  `vehicle_type` varchar(50) NOT NULL,
+  `vehicle_class` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `vehicles`
+--
+
+INSERT INTO `vehicles` (`vehicle_id`, `vehicle_number`, `vehicle_type`, `vehicle_class`) VALUES
+(1, 'GX-121-23', 'Toyota Camry', 1),
+(3, 'GX-121-24', 'Ford F150', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehicle_options`
+--
+
+CREATE TABLE `vehicle_options` (
+  `option_id` int(11) NOT NULL,
+  `option_description` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `vehicle_options`
+--
+
+INSERT INTO `vehicle_options` (`option_id`, `option_description`) VALUES
+(1, 'Car'),
+(2, 'Motorbike'),
+(3, 'Bicycle');
 
 --
 -- Indexes for dumped tables
@@ -203,6 +335,15 @@ ALTER TABLE `cart`
 --
 ALTER TABLE `customers`
   ADD PRIMARY KEY (`customer_id`);
+
+--
+-- Indexes for table `deliveries`
+--
+ALTER TABLE `deliveries`
+  ADD PRIMARY KEY (`delivery_id`),
+  ADD KEY `driver_id` (`driver_id`),
+  ADD KEY `customer_id` (`customer_id`),
+  ADD KEY `fk_provider_id` (`provider_id`);
 
 --
 -- Indexes for table `drivers`
@@ -242,6 +383,13 @@ ALTER TABLE `providers`
   ADD PRIMARY KEY (`provider_id`);
 
 --
+-- Indexes for table `ratings`
+--
+ALTER TABLE `ratings`
+  ADD PRIMARY KEY (`rating_id`),
+  ADD KEY `business_id` (`business_id`);
+
+--
 -- Indexes for table `roles`
 --
 ALTER TABLE `roles`
@@ -268,7 +416,14 @@ ALTER TABLE `users`
 --
 ALTER TABLE `vehicles`
   ADD PRIMARY KEY (`vehicle_id`),
-  ADD UNIQUE KEY `vehicle_number` (`vehicle_number`);
+  ADD UNIQUE KEY `vehicle_number` (`vehicle_number`),
+  ADD KEY `fk_vehicle_class` (`vehicle_class`);
+
+--
+-- Indexes for table `vehicle_options`
+--
+ALTER TABLE `vehicle_options`
+  ADD PRIMARY KEY (`option_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -278,7 +433,13 @@ ALTER TABLE `vehicles`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `deliveries`
+--
+ALTER TABLE `deliveries`
+  MODIFY `delivery_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `orders`
@@ -299,28 +460,40 @@ ALTER TABLE `payments`
   MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `ratings`
+--
+ALTER TABLE `ratings`
+  MODIFY `rating_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `services`
 --
 ALTER TABLE `services`
-  MODIFY `service_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `service_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `vehicles`
 --
 ALTER TABLE `vehicles`
-  MODIFY `vehicle_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `vehicle_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `vehicle_options`
+--
+ALTER TABLE `vehicle_options`
+  MODIFY `option_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
@@ -344,6 +517,14 @@ ALTER TABLE `cart`
 --
 ALTER TABLE `customers`
   ADD CONSTRAINT `customers_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `deliveries`
+--
+ALTER TABLE `deliveries`
+  ADD CONSTRAINT `deliveries_ibfk_1` FOREIGN KEY (`driver_id`) REFERENCES `drivers` (`driver_id`),
+  ADD CONSTRAINT `deliveries_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`),
+  ADD CONSTRAINT `fk_provider_id` FOREIGN KEY (`provider_id`) REFERENCES `providers` (`provider_id`);
 
 --
 -- Constraints for table `drivers`
@@ -380,6 +561,12 @@ ALTER TABLE `providers`
   ADD CONSTRAINT `providers_ibfk_1` FOREIGN KEY (`provider_id`) REFERENCES `users` (`user_id`);
 
 --
+-- Constraints for table `ratings`
+--
+ALTER TABLE `ratings`
+  ADD CONSTRAINT `ratings_ibfk_1` FOREIGN KEY (`business_id`) REFERENCES `users` (`user_id`);
+
+--
 -- Constraints for table `services`
 --
 ALTER TABLE `services`
@@ -390,6 +577,12 @@ ALTER TABLE `services`
 --
 ALTER TABLE `users`
   ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`);
+
+--
+-- Constraints for table `vehicles`
+--
+ALTER TABLE `vehicles`
+  ADD CONSTRAINT `fk_vehicle_class` FOREIGN KEY (`vehicle_class`) REFERENCES `vehicle_options` (`option_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
